20007a7709fb775f754734d59304a7fa4a166626
Omar Shishani <omarshishanischool@gmail.com>
2020-11-28 11:28:54 -0800

    Add navbar dropdown for logged in User

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navbar.css';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 const NavbarContainer = () => (
@@ -9,6 +9,13 @@ const NavbarContainer = () => (
     <Nav.Link as={Link} to="/recipeapp/">Home</Nav.Link>
     <Nav.Link as={Link} to="/recipeapp/recipe-upload">Recipe Upload</Nav.Link>
     <Nav.Link as={Link} to="/recipeapp/login">Login</Nav.Link>
+    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+    </NavDropdown>
   </Navbar>  
 )
 
