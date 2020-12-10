99ac5fcf7e29641259be9ab9dcb2b547a7deac1e
Omar Shishani <omarshishanischool@gmail.com>
2020-12-09 17:47:16 -0800

    Succeed in changing MyRecipes to a function component.

--- src/MyRecipes.js
+++ src/MyRecipes.js
@@ -1,75 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
-import {Container, ListGroup, Row, Col} from 'react-bootstrap';
+import { Container, ListGroup, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import global_url_variable from './global_url_variable';
 import './MyRecipes.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const url = global_url_variable;
 const urlMyRecipes = `${url}myrecipes`;
 
 axios.defaults.headers.common['Cache-Control'] = 'no-cache';
 
-class MyRecipes extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      recipes: []
-    }
-  }
-  componentDidMount() { 
+const MyRecipes = (props) => {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
     axios.get(urlMyRecipes)
     .then(res => {
       console.log(res.data);
-      this.setState({recipes: [...res.data]})
+      setRecipes([...res.data]);
     });
-  }
+  }, []);
   //set state with values from axios res.
-  render(){
-    const {recipes} = this.state;
-    let recipeListLeft;
-    let recipeListRight;
-    if (recipes.length > 0) {
-      recipeListLeft = recipes.map((x, ind) => {
-        if (ind % 2 === 0) {
-          return(
-            <ListGroup.Item key={ind} variant="primary" as={Link} to={`/recipeapp/recipe/${x.id}`}>{x.item}</ListGroup.Item>
-          )
-        } else {return null}
-      })
-      recipeListRight = recipes.map((x, ind) => {
-        if (ind % 2 !== 0) {
-          return(
-            <ListGroup.Item key={ind} variant="primary">{x.item}</ListGroup.Item>
-          )
-        } else {return null}
-      })
-    }
-    return(
-      <>
-        <Navbar 
-          loggedInUser={this.props.loggedInUser}
-          onLogout={this.props.onLogout}
-        />
-        <Container>
-          <h1>My Recipes</h1>
-          <Row>
-            <Col>
-              <ListGroup className="recipe-list-group">
-                {recipeListLeft}
-              </ListGroup>
-            </Col>
-            <Col>
-              <ListGroup className="recipe-list-group-right">
-                {recipeListRight}
-              </ListGroup>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
+  let recipeListLeft;
+  let recipeListRight;
+  if (recipes.length > 0) {
+    recipeListLeft = recipes.map((x, ind) => {
+      if (ind % 2 === 0) {
+        return(
+          <ListGroup.Item key={ind} variant="primary" as={Link} to={`/recipeapp/recipe/${x.id}`}>{x.item}</ListGroup.Item>
+        )
+      } else {return null}
+    })
+    recipeListRight = recipes.map((x, ind) => {
+      if (ind % 2 !== 0) {
+        return(
+          <ListGroup.Item key={ind} variant="primary">{x.item}</ListGroup.Item>
+        )
+      } else {return null}
+    })
   }
+  return(
+    <>
+      <Navbar 
+        loggedInUser={props.loggedInUser}
+        onLogout={props.onLogout}
+      />
+      <Container>
+        <h1>My Recipes</h1>
+        <Row>
+          <Col>
+            <ListGroup className="recipe-list-group">
+              {recipeListLeft}
+            </ListGroup>
+          </Col>
+          <Col>
+            <ListGroup className="recipe-list-group-right">
+              {recipeListRight}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default MyRecipes; 
