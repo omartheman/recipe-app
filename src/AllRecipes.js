8b396834e470198e6cc1b1ed38d09f5f250a9283
Omar Shishani <omarshishanischool@gmail.com>
2020-12-16 15:26:52 -0800

    Add AllRecipes

---
+++ src/AllRecipes.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import { Container, ListGroup, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import global_url_variable from './global_url_variable';
+import './MyRecipes.css';
+import { Link } from 'react-router-dom';
+
+const url = global_url_variable;
+const urlMyRecipes = `${url}myrecipes`;
+
+axios.defaults.headers.common['Cache-Control'] = 'no-cache';
+
+const AllRecipes = (props) => {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    axios.get(urlMyRecipes)
+    .then(res => {
+      console.log(res.data);
+      setRecipes([...res.data]);
+    });
+  }, []);
+  let recipeListLeft;
+  let recipeListRight;
+  if (recipes.length > 0) {
+    recipeListLeft = recipes.map((x, ind) => {
+      if (ind % 2 === 0) {
+        return(
+          <ListGroup.Item key={ind} variant="primary" as={Link} to={`/recipeapp/recipe/${x.id}`}>{x.item}</ListGroup.Item>
+        )
+      } else {return null}
+    })
+    recipeListRight = recipes.map((x, ind) => {
+      if (ind % 2 !== 0) {
+        return(
+          <ListGroup.Item key={ind} variant="primary" as={Link} to={`/recipeapp/recipe/${x.id}`}>{x.item}</ListGroup.Item>
+        )
+      } else {return null}
+    })
+  }
+  return(
+    <>
+      <Navbar 
+        loggedInUser={props.loggedInUser}
+        onLogout={props.onLogout}
+      />
+      <Container>
+        <h1>My Recipes</h1>
+        <Row>
+          <Col>
+            <ListGroup className="recipe-list-group">
+              {recipeListLeft}
+            </ListGroup>
+          </Col>
+          <Col>
+            <ListGroup className="recipe-list-group-right">
+              {recipeListRight}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default AllRecipes; 
