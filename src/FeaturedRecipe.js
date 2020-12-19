b2c31ce3a415610d76e5457a6bfad5b17b634259
Omar Shishani <omarshishanischool@gmail.com>
2020-12-19 12:08:28 -0800

    Add FeaturedRecipe.scss, FeaturedRecipe Link

--- src/FeaturedRecipe.js
+++ src/FeaturedRecipe.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import './FeaturedRecipe.css';
-import {Container, Image} from 'react-bootstrap';
+import './FeaturedRecipe.scss';
+import { Container, Image } from 'react-bootstrap';
 import axios from 'axios';
 import global_url_variable from './global_url_variable';
+import { Link } from 'react-router-dom';
 
 const url = global_url_variable;
 const urlFeatured = `${url}get-featured-recipe`;
@@ -21,7 +22,7 @@ class FeaturedRecipe extends React.Component{
   render(){
     const {featuredRecipe} = this.state;
     let featuredRecipeEl =
-      <Container>
+      <Container as={Link} to={`https://brittanyjewellneal.com/recipes/${featuredRecipe[0]}`}>
         <h3>Featured Recipe: {featuredRecipe[2]}</h3>
           <Image 
             className="featrd-rec-img" 
@@ -33,7 +34,7 @@ class FeaturedRecipe extends React.Component{
     ;
     if (url === "http://localhost:4000/recipeapp/recipeapp-server/") {
       featuredRecipeEl = 
-        <Container>
+        <Container className="featured-recipe-container" as={Link} to="https://unsplash.com/">
           <h3>Featured Recipe: Fried Eggs</h3>
           <Image className="featrd-rec-img" src="https://images.unsplash.com/photo-1582169505937-b9992bd01ed9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=698&q=80" rounded fluid/>
           <p>Lorem ipsum recipe shit</p>
