d9e2c9a641df809c3d5693e0587143eb0756c1b6
Omar Shishani <omarshishanischool@gmail.com>
2020-12-18 16:58:16 -0800

    Change name Navbar.css

--- src/NavbarContainer.js
+++ src/NavbarContainer.js
@@ -1,5 +1,5 @@
 import {React} from 'react';
-import './Navbar.css';
+import './NavbarContainer.css';
 import {Navbar, Nav, NavDropdown, Form, Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
