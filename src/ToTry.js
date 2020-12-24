e73b9e22ec0c4cf011ae353bebc12ef534f34b06
Omar Shishani <omarshishanischool@gmail.com>
2020-12-23 21:45:46 -0800

    Add hook function for window width to remove col titles for mobile

--- src/ToTry.js
+++ src/ToTry.js
@@ -128,6 +128,32 @@ function ToTry(props) {
   const handleItemInput = (e) => {
     setItem(e.target.value);
   }
+  function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  };
+  const size = useWindowSize();
   return(
     <>
       {confirmMsg && 
@@ -190,22 +216,24 @@ function ToTry(props) {
           <Button onClick={postRecipeToTry}>Submit</Button>
         </Form>
         <ListGroup className="to-try-list-container">
-          <ListGroup.Item variant="secondary">
-            <Row>
-              <Col>
-                <strong>Recipe</strong>
-              </Col>
-              <Col>
-                <strong>Link</strong>
-              </Col>
-              <Col>
-                <strong>Tags</strong>
-              </Col>
-              <Col>
-                <strong>Image</strong>
-              </Col>
-            </Row>
-          </ListGroup.Item>
+          {size.width >= 991 && 
+            <ListGroup.Item variant="secondary">
+              <Row>
+                <Col>
+                  <strong>Recipe</strong>
+                </Col>
+                <Col>
+                  <strong>Link</strong>
+                </Col>
+                <Col>
+                  <strong>Tags</strong>
+                </Col>
+                <Col>
+                  <strong>Image</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          }
           {tryItems && tryList}
         </ListGroup> 
       </Container>
