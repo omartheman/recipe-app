443b1558dd9c26582aa7e2c6b0c0a8141d4444a5
Omar Shishani <omarshishanischool@gmail.com>
2021-01-31 21:27:18 -0800

    Add salad button

--- src/HomeMainCont.js
+++ src/HomeMainCont.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './HomeMainCont.scss';
-import { ReactComponent as Diet } from './images/diet.svg';
+import { ReactComponent as Diet } from './images/salad-opt.svg';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const HomeMainCont = (props) => (
   <main className="container">
@@ -9,7 +11,19 @@ const HomeMainCont = (props) => (
       <p>
         Stash your culinary creations here for later use. Never hunt for a recipe again. Store the memories of that delightful dish.
       </p>
-      <Diet className="home-main-cont-food-icon"/>
+      <div className="home-main-cont-food-icon-container">
+        <Diet className="home-main-cont-food-icon"/>
+        <div className="home-main-cont-go-to-all-recipes-span-wrapper">
+            <Button
+              as={Link}
+              to="/recipeapp/all-recipes"
+              className="home-main-cont-go-to-all-recipes-span"
+              variant="success"
+            >
+              Go to All Recipes
+            </Button>
+        </div>
+      </div>
       <p className="home-intro-text-center">
         Bon Appétit!
       </p>
