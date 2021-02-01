21ba9da14dd20a43577f6b2cd8cf2d99271efd85
Omar Shishani <omarshishanischool@gmail.com>
2021-01-31 17:30:11 -0800

    Change order of "welcome-text"

--- src/HomeMainCont.js
+++ src/HomeMainCont.js
@@ -5,11 +5,11 @@ import { ReactComponent as Diet } from './images/diet.svg';
 const HomeMainCont = (props) => (
   <main className="container">
     <div className="welcome-text">
-      <Diet className="home-main-cont-food-icon"/>
       <h2>Welcome to The Recipe Cabinet</h2>
       <p>
         Stash your culinary creations here for later use. Never hunt for a recipe again. Store the memories of that delightful dish.
       </p>
+      <Diet className="home-main-cont-food-icon"/>
       <p className="home-intro-text-center">
         Bon Appétit!
       </p>
